ba1086d170e93647c60efa64aa7d961e64ff3f15
Randel Jiao <randeljiao@gmail.com>
2019-09-29 00:00:16 +0000

    Automatically backed up by Learn

---
+++ ger.rb
@@ -0,0 +1,13 @@
+lev_force = 6
+while lev_force < 10 
+puts "Wingardium Leviosa"
+lev_force += 1 
+end 
+
+puts "space"
+
+lev_force = 6
+until lev_force == 10 
+print "Wingardium Leviosa"
+lev_force += 1 
+end
